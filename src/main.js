3f8314fbc9a9758675c99f99997cc08b08a7da22
MahmudJion <mahmudh.jion02@gmail.com>
2021-07-06 18:24:07 +0600

    update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./routes/index";
-import * as firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/database";
 import store from "./store";
 import { BootstrapVue } from 'bootstrap-vue'
 
